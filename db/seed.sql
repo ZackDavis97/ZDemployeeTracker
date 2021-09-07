50cc10a1afbe42de6b33adf0a8ddd6da2202b9a2
Zack Davis <jantrezac95@gmail.com>
2021-09-07 16:16:10 -0500

    schema code and some minor index/seed

---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+USE workDB;
+
+INSERT INTO department (name)
+VALUES ("Engineering");
+INSERT INTO department (name)
+VALUES ("Finance");
+INSERT INTO department (name)
+VALUES ("Legal");
+INSERT INTO department (name)
+VALUES ("Sales");
